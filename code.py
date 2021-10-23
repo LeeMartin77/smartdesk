ab47ba45aa5f238b67a6cebe78442bb9cf30e605
Lee Martin <lee.martin@pliminus.co.uk>
2021-10-23 22:40:04 +0100

    Added: Scrolling Shift Register

--- code.py
+++ code.py
@@ -56,6 +56,46 @@ def update_display(display, declaration_to_display, declarations):
     display.text(f'Hello {declarations[declaration_to_display]}', 20, 20, 1)
     display.show()
 
+def setup_shift_register_pins():
+    ser_output = digitalio.DigitalInOut(board.GP6)
+    ser_output.direction = digitalio.Direction.OUTPUT
+    ser_output.value = False
+
+    rclk_output = digitalio.DigitalInOut(board.GP7)
+    rclk_output.direction = digitalio.Direction.OUTPUT
+    rclk_output.value = False
+
+    srclk_output = digitalio.DigitalInOut(board.GP8)
+    srclk_output.direction = digitalio.Direction.OUTPUT
+    srclk_output.value = False
+
+    output_enable = digitalio.DigitalInOut(board.GP10)
+    output_enable.direction = digitalio.Direction.OUTPUT
+    output_enable.value = False
+
+    shiftregister_clear = digitalio.DigitalInOut(board.GP11)
+    shiftregister_clear.direction = digitalio.Direction.OUTPUT
+    shiftregister_clear.value = True
+    shiftregister_clear.value = False
+    shiftregister_clear.value = True
+
+
+    return [ser_output, rclk_output, srclk_output, output_enable, shiftregister_clear]
+
+def increment_shift_register(shift_register_value, shift_register_pins):
+    shift_register_value = shift_register_value + 1
+    if shift_register_value > 7:
+        shift_register_value = 0
+    for i in range(7):
+        # print(f'{shift_register_value}:{i}')
+        shift_register_pins[2].value = False
+        shift_register_pins[0].value = i == shift_register_value 
+        shift_register_pins[2].value = True
+        shift_register_pins[2].value = False
+    shift_register_pins[1].value = True
+    shift_register_pins[1].value = False
+    return shift_register_value
+
 (left_pin, right_pin) = setup_input()
 
 display = setup_display()
@@ -63,11 +103,20 @@ display = setup_display()
 declaration_to_display = 0
 declarations = ['Friends', 'Daniel', 'Timothy']
 
+shift_register_pins = setup_shift_register_pins()
+
+loop_time = time.time()
+
+shift_register_value = 0
+
 # Main Loop
 while True:
     start_declaration = declaration_to_display
     declaration_to_display = input_handler(declaration_to_display, left_pin, right_pin)
     if declaration_to_display != start_declaration:
         update_display(display, declaration_to_display, declarations)
-
+    if time.time() > loop_time:
+        loop_time = time.time()
+        shift_register_value = increment_shift_register(shift_register_value, shift_register_pins)
+    
     
